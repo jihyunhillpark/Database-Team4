bebe736a85a8e1f161acc17ab564e31bc31b5313
jihyunhillpark <girl9505@naver.com>
2019-05-16 02:50:22 +0900

    Update playstore.sql
    restore table creation file

--- playstore.sql
+++ playstore.sql
@@ -52,12 +52,12 @@ CREATE TABLE IF NOT EXISTS books(
   posted_date DATE NOT NULL,
   PRIMARY KEY (`book_id`)
 );
-/*
+
 INSERT INTO user (email,password,name,birthday,nationality)VALUES('21600301@handong.edu','qkrwlgus', 'jihyunpark','1995.05.06','SouthKorea'),
 ('21500771@handong.edu','gkalswl', 'minjiha','1996.05.07','SouthKorea'),
 ('21300333@handong.edu','qkrgusdn', 'hyunwoopark','1994.03.01','USA'),
 ('21500172@handong.edu','rlacksrud', 'chankyungkim','1995.12.25','USA');
-*/
+
 
 INSERT INTO books (book_id, ISBN, title, author, price, page, genre, language, publisher, posted_date) VALUES
   (1, 9791189015572, 'Six wakes', 'Mur Lafferty', 9900 , 648, 'Fiction / Science Fiction / General', 'korean', 'Kyobobook MCP', 190425) ,
@@ -65,6 +65,4 @@ INSERT INTO books (book_id, ISBN, title, author, price, page, genre, language, p
   (3, 9788934995005, 'Fact Ful Nes', 'Hans Rosling', 11880, 572, 'Literary Collections / Essays Philosophy / General', 'korean', 'Co. kimyong', 190228),
   (4, 9791164130801, 'Bad Blood', 'John Carreyrou', 10080, 468, 'Business & Economics / General', 'korean', 'Wiseberry', 190415);
 
-/*
 INSERT INTO apps (`app_id`, `app_name`, `description`, `developer_id`, `url`, `price`, `downloaded_num`, `uploaded_date`, `latest_update`, `score`, `rating`, `current_version`, `whats_new`, `required_software`, `size`, `editors_choice`, `coming_soon`, `weekly_recommended`) VALUES ('2', 'Facebook', 'Keeping up with friends is faster and easier than ever. Share updates and photos, engage with friends and Pages, and stay connected to communities important to you. ', '2', 'https://play.google.com/store/apps/details?id=com.facebook.katana', 'FREE', '1,000,000,000+', '2010-01-01', '2019-05-14', '4.1 total 87,090,209 ', 'Rated for 12+ Parental Guidance Recommended', 'Varies with device', ' Improvements for reliability and speed', 'Varies with device', 'Varies with device', '0', '0', '0');
-*/
