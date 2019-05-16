f407d693ee70d32426b8313a87046003d0e2ebf2
jihyunhillpark <girl9505@naver.com>
2019-05-17 05:12:16 +0900

    Update playstore.sql

--- playstore.sql
+++ playstore.sql
@@ -87,7 +87,7 @@ INSERT INTO books (book_id, ISBN, title, author, price, page, genre, language, p
   (4, 9791164130801, 'Bad Blood', 'John Carreyrou', 10080, 468, 'Business & Economics / General', 'korean', 'Wiseberry', 190415);
 
 INSERT INTO apps (`app_id`, `app_name`, `description`, `developer_id`, `url`, `price`, `downloaded_num`, `uploaded_date`, `latest_update`, `score`, `rating`, `current_version`, `whats_new`, `required_software`, `size`, `editors_choice`, `coming_soon`, `weekly_recommended`) VALUES ('2', 'Facebook', 'Keeping up with friends is faster and easier than ever. Share updates and photos, engage with friends and Pages, and stay connected to communities important to you. ', '2', 'https://play.google.com/store/apps/details?id=com.facebook.katana', 'FREE', '1,000,000,000+', '2010-01-01', '2019-05-14', '4.1 total 87,090,209 ', 'Rated for 12+ Parental Guidance Recommended', 'Varies with device', ' Improvements for reliability and speed', 'Varies with device', 'Varies with device', '0', '0', '0');
-*/
+
 INSERT INTO `movies`(`movie_id`, `title`, `year`, `language`, `length`, `age_limit`, `subtitle`, `price`) VALUES 
 (1, 'How to train your dragon3', '2019.01.11', 'korean', 104, 0, NULL, 6500) ,
 (2, 'wonder', '2017.11.21', 'English', 113, 0, 'korean', 5000),
